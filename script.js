20e1610e9227854ad91675d855f49ff0554ba63d
felipe <felipe@felipes-MacBook-Pro.local>
2018-03-06 13:17:13 +0000

    test title

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-(function($) {
-  $('a[href^=mailto]').each(function() {
-    var href = $(this).attr('href');
-    $(this).click(function() {
-      var t;
-      var self = $(this);
+$( document ).ready(function() {
 
-      $(window).blur(function() {
-        // The browser apparently responded, so stop the timeout.
-        clearTimeout(t);
-      });
-
-      t = setTimeout(function() {
-        // The browser did not respond after 500ms, so open an alternative URL.
-        document.location.href = '...';
-      }, 500);
-    });
+  $(window).blur(function(e) {
+      $("title").text("Hey, come back");
   });
+
+  $(window).focus(function(e) {
+      $("title").text("Welcome back!");
+  });
+
 });
